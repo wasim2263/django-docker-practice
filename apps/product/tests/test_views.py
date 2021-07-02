d0fee2bab5ed0b3abfe7a0e997b43cdd1396840d
wasim <abdullahwasim42@gmail.com>
2021-07-02 23:58:07 +0600

    single product add test done

--- apps/product/tests/test_views.py
+++ apps/product/tests/test_views.py
@@ -3,6 +3,8 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from apps.product.models import Category
+
 
 class TestProductViews(TestCase):
     def setUp(self) -> None:
@@ -15,18 +17,23 @@ class TestProductViews(TestCase):
         user.set_password('wasim2263')
         user.save()
         self.client.login(username='abdullah', password='wasim2263')
-        self.product_list_url = reverse('product:product-list')
-        self.product_add_url = reverse('product:product-add')
 
     def test_product_list_get(self):
-        response = self.client.get(self.product_list_url)
+        product_list_url = reverse('product:product-list')
+        response = self.client.get(product_list_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'product/product-list.html')
 
     def test_single_product_post(self):
+        product_add_url = reverse('product:product-add', kwargs={'product_id': None})
+        category = Category.objects.create(name='test-category')
         product = {
-            ''
+            'code': 'test-code',
+            'name': 'test-name',
+            'unit_price': 50,
+            'unit_type': 'kg',
+            'stock': 100,
+            'category': category.id
         }
-        response = self.client.post(self.product_add_url)
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'product/product-list.html')
+        response = self.client.post(product_add_url, product)
+        self.assertEquals(response.status_code, 302)
